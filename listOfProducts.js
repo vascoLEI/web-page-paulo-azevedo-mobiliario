d70f210244736a77e67918cb60ecb1e15b502737
Vasco Azevedo <vasco12411@gmail.com>
2025-11-18 23:51:47 +0000

    Add one script so I don't have to add the products one at a time.

---
+++ listOfProducts.js
@@ -0,0 +1,59 @@
+const PRODUTOS = [
+  {
+    id: "espelho-redondo",
+    nome: "Espelho redondo",
+    imagem: "images/espelho redondo2.jpg",
+    categoria: "decoracao",
+    descricao: "Espelho decorativo com iluminação."
+  },
+  {
+    id: "cama-dourado",
+    nome: "Cama com Dourado",
+    imagem: "images/cama com detalhe dourado.jpg",
+    categoria: "quarto",
+    descricao: "Estrado e cabeceira estofados com apontamentos dourados."
+  },
+  {
+    id: "sofa-branco",
+    nome: "Sofá branco",
+    imagem: "images/sofá branco.jpg",
+    categoria: "sala",
+    descricao: "Sofá de canto em tecido claro, muito confortável."
+  },
+  {
+    id: "sala-jantar",
+    nome: "Sala de jantar",
+    imagem: "images/sala de jantar.jpg",
+    categoria: "sala",
+    descricao: "Conjunto de mesa e cadeiras para sala de jantar."
+  },
+  {
+    id: "sofa-2",
+    nome: "Sofá 2 lugares",
+    imagem: "images/sofa2.jpg",
+    categoria: "sala",
+    descricao: "Sofá compacto ideal para espaços mais pequenos."
+  },
+  {
+    id: "cama1",
+    nome: "Cama estofada",
+    imagem: "images/cama1.jpg",
+    categoria: "quarto",
+    descricao: "Cama estofada com arrumação."
+  },
+  {
+    id: "cama2",
+    nome: "Cama",
+    imagem: "images/cama2.jpg",
+    categoria: "quarto",
+    descricao: "Cama para quarto."
+  },
+
+  {
+    id: "aparador0",
+    nome: "Aparador",
+    imagem: "images/aparador0.jpg",
+    categoria: "aparador",
+    descricao: "Aparador branco."
+  },
+];
